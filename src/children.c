3f72f45dec4919efa01e5ff176533ab75a321c7a
Eunbi Cho <eucho@f0r2s7.codam.nl>
2023-04-12 17:12:56 +0200

    new grep_check

--- src/children.c
+++ src/children.c
@@ -6,7 +6,7 @@
 /*   By: eucho <eucho@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/27 09:54:06 by eucho         #+#    #+#                 */
-/*   Updated: 2023/04/12 16:26:24 by eucho         ########   odam.nl         */
+/*   Updated: 2023/04/12 17:11:33 by eucho         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,40 +28,80 @@ void	redirect(int in, int out)
 
 void	grep_check(t_pipex *pipex)
 {
-	int i;
-	int j;
-	char **args;
-	char *tmp;
+	int		len;
+	int		i;
+	int		args_num;
+	char	**args;
+	char	*tmp;
 
 	args = pipex->cmd_args;
-	tmp = ft_strdup("");
-	i = 0;
+	args_num = 0;
 	while (*args)
 	{
-		i++;
+		args_num++;
 		args++;
 	}
-	printf("number of args : %d\n", i);
-	if (i >= 3)
+	if (args_num >= 3)
 	{
-		j = 1;
-		while (j < i)
+		i = 1;
+		len = 0;
+		while (args[i] != NULL)
 		{
-			tmp = ft_strjoin(args[j], " ");
-			printf("tmp : %s\n", tmp);
-			ft_strjoin(tmp, args[j + 1]);
-			j++;
+			len += ft_strlen(args[i]);
+			i++;
+		}
+		tmp = malloc(sizeof(char) * (len + 1));
+		i = 1;
+		while (args[i] != NULL)
+		{
+			ft_strjoin(tmp, args[i]);
+			free(args[i]);
+			i++;
 		}
 		pipex->cmd_args[1] = tmp;
 		free(tmp);
-	}
-	j = 2;
-	while (j < i)
-	{
-		free(pipex->cmd_args[j]);
-		j++;
+		i = 2;
+		while (pipex->cmd_args[i] != NULL)
+		{
+			free(pipex->cmd_args[i]);
+			i++;
+		}
 	}
 }
+
+// void	grep_check(t_pipex *pipex)
+// {
+// 	int		i;
+// 	int		j;
+// 	char	**args;
+// 	char	*tmp;
+
+// 	args = pipex->cmd_args;
+// 	i = 0;
+// 	while (*args)
+// 	{
+// 		i++;
+// 		args++;
+// 	}
+// 	if (i >= 3)
+// 	{
+// 		j = 1;
+// 		printf("args : %s\n", args[j]);
+// 		tmp = args[j];
+// 		while (j < i)
+// 		{
+// 			ft_strjoin(tmp, " ");
+// 			ft_strjoin(tmp, args[j + 1]);
+// 			j++;
+// 		}
+// 	}
+// 	j = 2;
+// 	while (j < i)
+// 	{
+// 		free(pipex->cmd_args[j]);
+// 		j++;
+// 	}
+// }
 
 void	child_1(t_pipex pipex, char *argv[], char *envp[])
 {
