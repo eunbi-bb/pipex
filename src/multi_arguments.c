85258b62c74de675c739135c4b4fdd94eedd8f94
eunbi-bb <eunbicho1995@gmail.com>
2023-04-18 04:40:13 +0200

    multi_args func is working

--- src/multi_arguments.c
+++ src/multi_arguments.c
@@ -7,35 +7,35 @@ void	*protec(void *ptr)
 	return (ptr);
 }
 
-static char	*string_concat(char *s1, char *s2)
-{
-	int i;
-	int j;
-	int	len_1;
-	int	len_2;
-	char	*str;
+// static char	*string_concat(char *s1, char *s2)
+// {
+// 	int i;
+// 	int j;
+// 	int	len_1;
+// 	int	len_2;
+// 	char	*str;
 
-	if (!s1 || !s2)
-		return (NULL);
-	len_1 = ft_strlen(s1);
-	len_2 = ft_strlen(s2);
-	str = malloc(sizeof(char) * (len_1 + len_2 + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (i < len_1)
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (j < len_2)
-	{
-		str[i++] = s2[j];
-		j++;
-	}
-	str[i] = '\0';
-	return (str);
-}
+// 	if (!s1 || !s2)
+// 		return (NULL);
+// 	len_1 = ft_strlen(s1);
+// 	len_2 = ft_strlen(s2);
+// 	str = malloc(sizeof(char) * (len_1 + len_2 + 1));
+// 	if (!str)
+// 		return (NULL);
+// 	i = 0;
+// 	while (i < len_1)
+// 	{
+// 		str[i] = s1[i];
+// 		i++;
+// 	}
+// 	while (j < len_2)
+// 	{
+// 		str[i++] = s2[j];
+// 		j++;
+// 	}
+// 	str[i] = '\0';
+// 	return (str);
+// }
 
 void	multiple_args(char **args, char **new_args, int new_args_size)
 {
@@ -51,8 +51,8 @@ void	multiple_args(char **args, char **new_args, int new_args_size)
 			j++;
 		else if (new_args[j] && new_args[j][0] == '\'')
 		{
-			string_concat(new_args[j], " ");
-			string_concat(new_args[j], args[i]);
+			new_args[j] = ft_strjoin(new_args[j], " ");
+			new_args[j] = ft_strjoin(new_args[j], args[++i]);
 			if (new_args[j][ft_strlen(new_args[j]) - 1] == '\'')
 			{
 				new_args[j] = ft_substr(new_args[j], 1, ft_strlen(new_args[j]) - 2);
@@ -61,12 +61,12 @@ void	multiple_args(char **args, char **new_args, int new_args_size)
 		i++;
 	}
 	new_args[j + 1] = NULL;
-	// int n = 0;
-	// while (new_args[n])
-	// {
-	// 	printf("new_args[%d]: %s\n", n, new_args[n]);
-	// 	n++;
-	// }
+	int n = 0;
+	while (new_args[n])
+	{
+		printf("new_args[%d]: %s\n", n, new_args[n]);
+		n++;
+	}
 }
 
 // void	multiple_args(char **args, char **new_args)
